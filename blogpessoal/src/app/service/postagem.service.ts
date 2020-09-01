b2c1717f47507697ed1d7c9343e9a50bb019d64a
Hellen Albuquerque <hellenn.albuu@gmail.com>
2020-09-01 18:39:21 -0300

    Get

--- blogpessoal/src/app/service/postagem.service.ts
+++ blogpessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+  getAllPostagens() {
+    return this.http.get('http://localhost:9000/postagens', this.token)
+  }
+
+
 }
--- blogpessoal/src/app/service/tema.service.ts
+++ blogpessoal/src/app/service/tema.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {  HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+getAllTemas() {
+  return this.http.get('http://localhost:9000/tema', this.token)
 }
+
+getByIdTema(id: number) {
+  return this.http.get(`http://localhost:9000/tema/${id}`, this.token)
+} 
+
+}
+
