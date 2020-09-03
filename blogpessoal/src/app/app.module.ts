f86019616b7517e0c08b993815e9504b0a61c255
Hellen Albuquerque <hellenn.albuu@gmail.com>
2020-09-03 16:37:02 -0300

    Implementação Put-Delete

--- blogpessoal/src/app/app.module.ts
+++ blogpessoal/src/app/app.module.ts
@@ -15,6 +15,10 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+import { PutTemaComponent } from './put-tema/put-tema.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +30,11 @@ import { PostTemaComponent } from './post-tema/post-tema.component';
     PerfilComponent,
     LoginComponent,
     CadastroComponent,
-    PostTemaComponent
+    PostTemaComponent,
+    PutPostagemComponent,
+    PutTemaComponent,
+    DeletePostagemComponent,
+    DeleteTemaComponent
   ],
   imports: [
     BrowserModule,
--- blogpessoal/src/app/service/postagem.service.ts
+++ blogpessoal/src/app/service/postagem.service.ts
@@ -15,10 +15,25 @@ export class PostagemService {
   }
 
   getAllPostagens() {
-    return this.http.get('http://localhost:8080/postagens', this.token)
+    return this.http.get('http://localhost:9000/postagens', this.token)
   }
 
- postPostagem(postagem: Postagem) {
-   return this.http.post('http://localhost:9000/postagens', postagem, this.token)
- }
+  getByIdPostagem(id: number) {
+    return this.http.get(`http://localhost:9000/postagens/${id}`, this.token)
+
+  }
+
+  postPostagem(postagem: Postagem) {
+    return this.http.post('http://localhost:9000/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem) {
+    return this.http.put('http://localhost:9000/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number) {
+    return this.http.delete(`http://localhost:9000/postagens/${id}`, this.token)
+  }
+
+
 }
